97523b78bf95b51390b245c46776f0428c9e48a1
dorms <dormsil@gmail.com>
2020-05-09 15:07:58 +0300

    app

--- plantbot/views.py
+++ plantbot/views.py
@@ -1,7 +1,6 @@
 from flask import request, jsonify, render_template, session, redirect, url_for, make_response
 from plantbot import app
 from plantbot.plant_identify import allowed_image, allowed_image_filesize, send_image
-from werkzeug.utils import secure_filename
 from plantbot.dialogflow import detect_intent_texts, get_response
 import os, base64, json
 from .model import get_plant, get_plants
@@ -37,34 +36,28 @@ def upload_image(**kwargs):
 
                 if allowed_image(image.filename):
 
-                    filename = secure_filename(image.filename)
-                    # image.save(os.path.join(app.config["IMAGE_UPLOADS"], filename))
-
-                    # with open(app.config["IMAGE_UPLOADS"]+"/"+filename, "rb") as file:
-                    # with open(image, "rb") as file:
                     image = [base64.b64encode(image.read()).decode("ascii")]
 
                         
-                    response = send_image(image)
-                    # plant_identify = {'id': 14143522, 'plant_name': 'Rosmarinus officinalis', 'plant_details': {'scientific_name': 'Rosmarinus officinalis', 'structured_name': {'genus': 'rosmarinus', 'species': 'officinalis'}, 'common_names': ['Rosemary', 'Salvia rosmarinus', 'Anthos'], 'url': 'http://en.wikipedia.org/wiki/Rosmarinus_officinalis', 'wiki_description': {'value': 'Salvia rosmarinus, commonly known as rosemary, is a woody, perennial herb with fragrant, evergreen, needle-like leaves and white, pink, purple, or blue flowers, native to the Mediterranean region. Until 2017, it was known by the scientific name Rosmarinus officinalis, now a synonym.\nIt is a member of the mint family Lamiaceae, which includes many other herbs. The name "rosemary" derives from Latin ros marinus ("dew of the sea"). The plant is also sometimes called anthos, from the ancient Greek word ἄνθος, meaning "flower". Rosemary has a fibrous root system.', 'citation': 'http://en.wikipedia.org/wiki/Rosmarinus_officinalis', 'license_name': 'CC BY-SA 3.0', 'license_url': 'https://creativecommons.org/licenses/by-sa/3.0/'}, 'taxonomy': {'kingdom': 'Plantae', 'phylum': 'Tracheophyta', 'class': 'Magnoliopsida', 'order': 'Lamiales', 'family': 'Lamiaceae', 'genus': 'Rosmarinus'}}, 'probability': 0.8978623588873735, 'confirmed': False, 'similar_images': [{'id': '84a3079304551a37b1a6b762f0bb09ff', 'similarity': 0.8775884678848236, 'url': 'https://storage.googleapis.com/plant_id_images/similar_images/2019_05/images/Rosmarinus officinalis/84a3079304551a37b1a6b762f0bb09ff.jpg', 'url_small': 'https://storage.googleapis.com/plant_id_images/similar_images/2019_05/images/Rosmarinus officinalis/84a3079304551a37b1a6b762f0bb09ff.small.jpg'}, {'id': '5ab0817f2cf6a205195d1ce1740bfb63', 'similarity': 0.8766923338234861, 'url': 'https://storage.googleapis.com/plant_id_images/similar_images/2019_05/images/Rosmarinus officinalis/5ab0817f2cf6a205195d1ce1740bfb63.jpg', 'url_small': 'https://storage.googleapis.com/plant_id_images/similar_images/2019_05/images/Rosmarinus officinalis/5ab0817f2cf6a205195d1ce1740bfb63.small.jpg'}]}
-                    plant_identify =response['suggestions'][0]
-                    print(plant_identify)
+                    # response = send_image(image)
+                    # plant_identify =response['suggestions'][0]
+                    # print(plant_identify)
                     
-                    for i, plant in enumerate(get_plants()) :
-                        print(i)
-                        if session.get('plant_index') is not None:
-                            break
-                        print(plant)
-                        names = plant_identify['plant_name'].split(' ')
-                        for name in names:
-                            print(name)
-                            if name.lower() in  plant['plant name'].lower():
-                                print('in if ==========')
-                                session['plant_index'] = (i+1)
-                                break
+                    # for i, plant in enumerate(get_plants()) :
+                    #     print(i)
+                    #     if session.get('plant_index') is not None:
+                    #         break
+                    #     print(plant)
+                    #     names = plant_identify['plant_name'].split(' ')
+                    #     for name in names:
+                    #         print(name)
+                    #         if name.lower() in  plant['plant name'].lower():
+                    #             print('in if ==========')
+                    #             session['plant_index'] = (i+1)
+                    #             break
                             
-                    # session['plant_index'] = 2
-                    # os.remove(os.path.join(app.config["IMAGE_UPLOADS"], filename))    
+                    session['plant_index'] = 2
+                      
                     if session.get('plant_index') == None:   
                         return render_template("upload_image.html", error="not found plant")
                         
