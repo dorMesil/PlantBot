9f2061981ff8c10156f25e4f3a12137c2fd5b393
dorms <dormsil@gmail.com>
2020-06-08 12:19:15 +0300

    app

--- plantbot/config.py
+++ plantbot/config.py
@@ -6,6 +6,5 @@ MONGO_URI = 'mongodb+srv://admin:admin1234@cluster0-rsh71.mongodb.net/test?retry
 ALLOWED_IMAGE_EXTENSIONS = ["JPEG", "JPG", "PNG"]
 MAX_IMAGE_FILESIZE = 100 * 1024 * 1024
 API_KEY ='uPNlLvtXfhpnqQqEHuQx8jwwxvWUPW7ix0U9aIFGlcsiOdKYrd'
-
 SECRET_KEY = b'dor9dor'
 
