bafad780cdafa60170984d5843c1722a189f804c
dorms <dormsil@gmail.com>
2020-06-08 11:33:39 +0300

    app

--- plantbot/__init__.py
+++ plantbot/__init__.py
@@ -3,6 +3,7 @@ from pymongo import MongoClient
 import os
 
 # os.environ["FLASK_ENV"] = 'development'
+
 app = Flask(__name__,template_folder="templates",static_folder="static", instance_relative_config=True)
 
 app.config.from_object('plantbot.config')
