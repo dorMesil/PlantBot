6c3849b75d4db41c3787ef79f43eed3fe26c5e64
dorms <dormsil@gmail.com>
2020-06-08 11:26:28 +0300

    app

--- plantbot/__init__.py
+++ plantbot/__init__.py
@@ -5,7 +5,6 @@ import os
 # os.environ["FLASK_ENV"] = 'development'
 app = Flask(__name__,template_folder="templates",static_folder="static", instance_relative_config=True)
 
-
 app.config.from_object('plantbot.config')
 
 client = MongoClient(app.config["MONGO_URI"])
