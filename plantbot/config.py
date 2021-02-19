6994283d94daa3f0b9fb0aeed26a85a9a970e04d
dorMesil <49335120+dorMesil@users.noreply.github.com>
2021-02-19 12:54:05 +0200

    Delete config.py

--- plantbot/config.py
+++
@@ -1,9 +0,0 @@
-import  os
-
-DEBUG = True
-MONGO_URI = 'mongodb+srv://admin:admin1234@cluster0-rsh71.mongodb.net/test?retryWrites=true&w=majority' 
-ALLOWED_IMAGE_EXTENSIONS = ["JPEG", "JPG", "PNG"]
-MAX_IMAGE_FILESIZE = 100 * 1024 * 1024
-API_KEY ='uPNlLvtXfhpnqQqEHuQx8jwwxvWUPW7ix0U9aIFGlcsiOdKYrd'
-SECRET_KEY = b'dor9dor'
-
